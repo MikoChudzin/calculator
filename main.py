959ccdb1ac19234d9769207810325b656f57a1f0
Miko Chudzin <miko.chudzinski@gmail.com>
2024-07-06 20:14:01 +0200

    Add basic calculator functions

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ import sys
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="logfile.log")
 
-operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
-var1 = input("Podaj składnik 1:")
-var2 = input("Podaj składnik 2:")
 
-logging.debug(f"{operation} {var1} i {var2}")
-print("Wynik:")
+if __name__ == "__main__":
+    operation = input("Podaj działanie, posługując się odpowiednią liczbą: \n1. Dodawanie, \n2. Odejmowanie, \n3. Mnożenie, \n4. Dzielenie: ")
+    var1 = float(input("Podaj składnik 1:"))
+    var2 = float(input("Podaj składnik 2:"))
+    
+    if operation == "1":
+        operate = "Dodaję"
+        result = var1 + var2
+    elif operation == "2":
+        operate = "Odejmuję"
+        result = var1 - var2
+    elif operation == "3":
+        operate = "Mnożę"
+        result = var1 * var2
+    elif operation == "4":
+        operate = "Dzielę"
+        result = var1 / var2
+        
+    logging.debug("%s %s i %s" % operate, var1, var2)
+    print(f"Wynik: {result}")
