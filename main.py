dcbca9cb1b72b4fd18dd9015aba47a14f2c86618
Miko Chudzin <miko.chudzinski@gmail.com>
2024-07-06 22:18:55 +0200

    Add multiple variable support for addition and multiplication, Add ensurance that input is numeric

--- main.py
+++ main.py
@@ -3,23 +3,68 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="logfile.log")
 
 
-if __name__ == "__main__":
-    operation = input("Podaj działanie, posługując się odpowiednią liczbą: \n1. Dodawanie, \n2. Odejmowanie, \n3. Mnożenie, \n4. Dzielenie: ")
-    var1 = float(input("Podaj składnik 1:"))
-    var2 = float(input("Podaj składnik 2:"))
+
+def calculator(op,*args):
+    if op == "1":
+        result = 0
+        for i in range(0,len(args[0])):
+            result += args[0][i]
+        return result
+    elif op == "2":
+        return args[0][0]-args[0][1]
+    elif op == "3":
+        result = 1
+        for i in range(0,len(args[0])):
+            result *= args[0][i]
+        return result
+    elif op == "4":
+        return args[0][0] / args[0][1]
+    else:
+        return None
     
+def get_vars(a):
+    vars = []
+    if a == "many":
+        i = 1
+        while True:
+            add = input(f"Podaj składnik {i} (zatwierdź puste, jeśli nie masz więcej składników): ")
+            if add == "":
+                break
+            else:
+                if add.replace(".","").isnumeric():
+                    vars.append(float(add))
+                else:
+                    logging.debug("Jedna z podanych wartości nie jest liczbą.")
+                    sys.exit()
+            i += 1
+        return vars
+    elif a == "two":
+        for i in range(0,2):
+            add = input(f"Podaj składnik {i+1}: ")
+            if add.replace(".","").isnumeric():
+                vars.append(float(add))
+            else:
+                logging.debug("Jedna z podanych wartości nie jest liczbą.")
+                sys.exit()
+        return vars
+
+if __name__ == "__main__":
+    operation = input(
+        "Podaj działanie, posługując się odpowiednią liczbą:"
+        "\n1. Dodawanie, 2. Odejmowanie, 3. Mnożenie, 4. Dzielenie: "
+        )
     if operation == "1":
         operate = "Dodaję"
-        result = var1 + var2
+        vars = get_vars("many")
     elif operation == "2":
         operate = "Odejmuję"
-        result = var1 - var2
+        vars = get_vars("two")
     elif operation == "3":
         operate = "Mnożę"
-        result = var1 * var2
+        vars = get_vars("many")
     elif operation == "4":
         operate = "Dzielę"
-        result = var1 / var2
-        
-    logging.debug("%s %s i %s" % (operate, var1, var2))
-    print(f"Wynik: {result}")
+        vars = get_vars("two")
+    debug_msg = f"{operate} " + " i ".join(str(var) for var in vars)
+    logging.debug(debug_msg) #', '.join(str(x) for x in list)
+    print(f"Wynik: {calculator(operation,vars)}")
