8f4246aa3b56b5e4d6693daa88ebd6f72bcb3968
Miko Chudzin <miko.chudzinski@gmail.com>
2024-07-11 00:52:26 +0200

    Add separate functions for all operations, Add function with a dictionary of op functions

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def calculator(op,*args):
     """
     Performs a mathematic operation based on first arg
     on the rest of the arguments.
-    Supports multiple args for addition and multiplication
+    Supports multiple args for next_varition and multiplication
     Supports two args for subtraction and division
     Arguments:
     str op: "1","2","3","4"
@@ -31,10 +31,38 @@ def calculator(op,*args):
     else:
         return None
     
+def dict_calculator(op,*args):
+    """
+    op_dict = {
+        "1" : [result ]
+    }
+    
+    """
+    
+    pass
+
+def addition(*args):
+    result = 0
+    for i in range(0,len(args)):
+        result += args[i]
+    return result
+
+def subtraction(arg1, arg2):
+    return arg1 - arg2
+
+def multiplication(*args):
+    result = 1
+    for i in range(0,len(args)):
+        result *= args[i]
+    return result
+
+def division(arg1, arg2):
+    return arg1 / arg2
+    
 def get_vars(a):
     """
-    Prompts the user to input variables based
-    on the operation chosen
+    Prompts the user to input variables
+    based on the operation chosen
     Arguments:
     str a: "many","two"
     """
@@ -42,12 +70,12 @@ def get_vars(a):
     if a == "many":
         i = 1
         while True:
-            add = input(f"Podaj składnik {i} (zatwierdź puste, jeśli nie masz więcej składników): ")
-            if add == "":
+            next_var = input(f"Podaj składnik {i} (zatwierdź puste, jeśli nie masz więcej składników): ")
+            if next_var == "":
                 break
             else:
-                if add.replace(".","").isnumeric():
-                    vars.append(float(add))
+                if next_var.replace(".","").isnumeric():
+                    vars.append(float(next_var))
                 else:
                     logging.debug("Jedna z podanych wartości nie jest liczbą.")
                     sys.exit()
@@ -55,9 +83,9 @@ def get_vars(a):
         return vars
     elif a == "two":
         for i in range(0,2):
-            add = input(f"Podaj składnik {i+1}: ")
-            if add.replace(".","").isnumeric():
-                vars.append(float(add))
+            next_var = input(f"Podaj składnik {i+1}: ")
+            if next_var.replace(".","").isnumeric():
+                vars.append(float(next_var))
             else:
                 logging.debug("Jedna z podanych wartości nie jest liczbą.")
                 sys.exit()
@@ -69,18 +97,32 @@ if __name__ == "__main__":
         "Podaj działanie, posługując się odpowiednią liczbą:"
         "\n1. Dodawanie, 2. Odejmowanie, 3. Mnożenie, 4. Dzielenie: "
         )
-    if operation == "1":
-        operate = "Dodaję"
-        vars = get_vars("many")
-    elif operation == "2":
-        operate = "Odejmuję"
-        vars = get_vars("two")
-    elif operation == "3":
-        operate = "Mnożę"
-        vars = get_vars("many")
-    elif operation == "4":
-        operate = "Dzielę"
-        vars = get_vars("two")
-    debug_msg = f"{operate} " + " i ".join(str(var) for var in vars)
-    logging.debug(debug_msg) #', '.join(str(x) for x in list)
-    print(f"Wynik: {calculator(operation,vars)}")
+
+    # if operation == "1":
+    #     operate = "Dodaję"
+    #     vars = get_vars("many")
+    # elif operation == "2":
+    #     operate = "Odejmuję"
+    #     vars = get_vars("two")
+    # elif operation == "3":
+    #     operate = "Mnożę"
+    #     vars = get_vars("many")
+    # elif operation == "4":
+    #     operate = "Dzielę"
+    #     vars = get_vars("two")
+    # debug_msg = f"{operate} " + " i ".join(str(var) for var in vars)
+    # logging.debug(debug_msg) #', '.join(str(x) for x in list)
+    # print(f"Wynik: {calculator(operation,vars)}")
+
+    # op_dict = {
+    #     "1" : ["Dodaję",   get_vars("many")],
+    #     "2" : ["Odejmuję", get_vars("two")],
+    #     "3" : ["Mnożę",    get_vars("many")],
+    #     "4" : ["Dzielę",   get_vars("two")]
+    # }
+    # vars = op_dict[operation][1]
+    # debug_msg = f"{op_dict[operation][0]} " + " i ".join(str(var) for var in vars)
+    # logging.debug(debug_msg) #', '.join(str(x) for x in list)
+    # print(f"Wynik: {calculator(operation,vars)}")
+    print(addition(2,3,5), multiplication(5,5,5))
+    print(subtraction(20,10), division(10,2))
