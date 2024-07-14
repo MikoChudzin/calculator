e187fcd6104f45be1e334f08753c8727b05b32b0
Miko Chudzin <miko.chudzinski@gmail.com>
2024-07-14 17:54:30 +0200

    Compartmentalized get_var() and main()

--- main.py
+++ main.py
@@ -2,20 +2,33 @@ import sys
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="logfile.log")
     
-def dict_calculator(op,args):
+def dict_calculator(op):
     """
     Chooses a function to execute based on input
     Arguments:
     str op: "1" "2" "3" "4"
-    list of floats args
     """
     results_dict = {
-        "1" : ["Dodaję", addition(args)],
-        "2" : ["Odejmuję", subtraction(args)],
-        "3" : ["Mnożę", multiplication(args)],
-        "4" : ["Dzielę", division(args)]
+        "1" : addition,
+        "2" : subtraction,
+        "3" : multiplication,
+        "4" : division
     }
     return results_dict[op]
+
+def dict_operation(op):
+    """
+    Returns a string naming the kind of operation being executed
+    Arguments:
+    str op: "1" "2" "3" "4"
+    """
+    operation_dict = {
+        "1" : "Dodaję",
+        "2" : "Odejmuję",
+        "3" : "Mnożę",
+        "4" : "Dzielę"
+    }
+    return operation_dict[op]
 
 def addition(args):
     result = 0
@@ -34,48 +47,52 @@ def multiplication(args):
 
 def division(args):
     return args[0] / args[1]
-    
-def get_vars(a):
-    """
-    Prompts the user to input variables
-    based on the operation chosen
-    Arguments:
-    str a: "1" "2" "3" "4"
-    """
-    vars = []
-    if a == "1" or a == "3":
-        i = 1
-        while True:
-            next_var = input(f"Podaj składnik {i} (zatwierdź puste, jeśli nie masz więcej składników): ")
-            if next_var == "":
-                break
-            else:
-                if next_var.replace(".","").isnumeric():
-                    vars.append(float(next_var))
-                else:
-                    logging.debug("Jedna z podanych wartości nie jest liczbą.")
-                    sys.exit()
-            i += 1
-        return vars
-    elif a == "2" or a == "4":
-        for i in range(0,2):
-            next_var = input(f"Podaj składnik {i+1}: ")
-            if next_var.replace(".","").isnumeric():
-                vars.append(float(next_var))
-            else:
-                logging.debug("Jedna z podanych wartości nie jest liczbą.")
-                sys.exit()
-        return vars
 
-if __name__ == "__main__":
-    operation = input(
+def check_num(arg):
+    if arg.replace(".","").isnumeric():
+        return True
+    else:
+        logging.debug("Jedna z podanych wartości nie jest liczbą.")
+        sys.exit()
+    
+def get_operation():
+    op = input(
         "Podaj działanie, posługując się odpowiednią liczbą:"
         "\n1. Dodawanie, 2. Odejmowanie, 3. Mnożenie, 4. Dzielenie: "
         )
+    if op in "1234":
+        return op
+    else:
+        logging.debug("Podana wartość niezgodna z instrukcją.")
+        sys.exit()
 
-    vars = get_vars(operation)
-    results = dict_calculator(operation, vars)
-    debug_msg = f"{results[0]} " + " i ".join(str(var) for var in vars)
+def get_base_vars():
+    base_vars = []
+    for i in range(0,2):
+        next_var = input(f"Podaj składnik {i+1}: ")
+        if check_num(next_var):
+            base_vars.append(float(next_var))
+    return base_vars
+
+def get_add_vars():
+    add_vars = []
+    while True:
+        next_var = input(f"Podaj następny składnik, lub wciśnij Enter aby zakończyć: ")
+        if next_var == "":
+            break
+        else:
+            if check_num(next_var):
+                add_vars.append(float(next_var))
+    return add_vars
+
+if __name__ == "__main__":
+    operation = get_operation()
+    vars = get_base_vars()
+    if operation in "13":
+        vars += get_add_vars()
+    
+    results = dict_calculator(operation)(vars)
+    debug_msg = f"{dict_operation(operation)} " + " i ".join(str(var) for var in vars)
     print(debug_msg)
     logging.debug(debug_msg)
-    print(f"Wynik: {results[1]}")
+    print(f"Wynik: {results}")
